dac6292a4d24f1bb1ac9e1917cf0a24221a7f859
Animesh Gaur <32695073+ani8222@users.noreply.github.com>
2018-08-31 18:21:56 -1200

    title

--- contact_process.php
+++ contact_process.php
@@ -17,7 +17,7 @@
     $logo = 'http://srthemes.com/demo/ramirez/image/logo.png';
     $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Ramirez Mail</title></head><body>";
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Animesh Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
